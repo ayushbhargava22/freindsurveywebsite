712fcf66647de6707b328cf1f00b60d008832696
ayush bhargava <57057106+ayushbhargava22@users.noreply.github.com>
2020-06-01 19:40:05 +0530

    Add files via upload

---
+++ techquetoquiz-user/js/main.js
@@ -0,0 +1,139 @@
+document.querySelectorAll('#option-2').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id2")
+        var box1_2 = document.getElementById("question-block-id3")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+        
+    })
+  })
+document.querySelectorAll('#option-1').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id")
+        var box1_2 = document.getElementById("question-block-id2")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-3').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id3")
+        var box1_2 = document.getElementById("question-block-id4")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-4').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id4")
+        var box1_2 = document.getElementById("question-block-id5")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-5').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id5")
+        var box1_2 = document.getElementById("question-block-id6")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-6').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id6")
+        var box1_2 = document.getElementById("question-block-id7")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-7').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id7")
+        var box1_2 = document.getElementById("question-block-id8")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-8').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id8")
+        var box1_2 = document.getElementById("question-block-id9")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+document.querySelectorAll('#option-9').forEach(item => {
+    item.addEventListener('click', event => {
+        var box1_1 = document.getElementById("question-block-id9")
+        var box1_2 = document.getElementById("question-block-id10")
+        if(box1_1.style.display=="none" && box1_2.style.display == "none")
+        {
+            box1_1.style.display = "block";
+            box1_2.style.display = "block";
+        }
+        else{
+            box1_1.style.display = "none"
+            box1_2.style.display = "block"
+        }
+    })
+})
+
+
+
